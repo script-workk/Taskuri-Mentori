e4fb0e9938146b530e02e3d57ceaefcd27aa7e6f
Ion Tinea <script.workk@gmail.com>
2022-01-23 22:49:35 +0300

    -Am corectat observatiile indicate de Mentor. -Am facut curat in cod. -Am adaugat adaugat cod cu animatie mai potrivita cu originalul, dar care trebu sa o adaptez sub proiect

--- Exercise 1/js/main.js
+++ Exercise 1/js/main.js
@@ -1,10 +1,3 @@
-// const trigger = document.getElementById('toggle');
-// const box = document.getElementById('menu');
-
-// toggle.addEventListener('click', function() {
-//   box.classList.toggle('active');
-// });
-
 const btnMenuOpen = document.querySelector(".burger-icon");
 const btnMenuClose = document.querySelector(".close-nav-icon");
 const menu = document.querySelector(".menu");
@@ -16,9 +9,6 @@ btnMenuOpen.addEventListener("click", () => {
   btnMenuClose.style.display = "block";
   btnMenuClose.style.color = "#ffffff";
   btnMenuClose.classList.add("icon-animation");
-  // btnMenuClose.style.transform = "rotate(0deg)";
-  // btnMenuClose.style.transition = "all 5s easy";
-  // btnMenuClose.style.animationDelay = "2s";
 });
 
 btnMenuClose.addEventListener("click", () => {
@@ -39,3 +29,36 @@ navLink.forEach((item) => {
     }
   };
 });
+
+const authorText = document.querySelector(".author-text");
+
+const addText = (text) => {
+  const textContent = document.createElement("p");
+  textContent.textContent = text;
+  authorText.appendChild(textContent);
+};
+addText(
+  `Maybe it was the other George Michael. You know, the singer-songwriter. This was a big get for God. I'm a scholar. I enjoy scholarly pursuits. Suddently playing wits yourself is a scholarly pursuit? Wow. We're just blowing through nap time, aren't we? I need a face passport, preferably to France... I like the way they think. Butterscotch! Want a lick?`
+);
+
+addText(
+  `I made a huge tiny mistake. What's gotten into you? Have you been eating cheese? one of the guys told me to take my head out of my bottom and get back to work... my bottom! Hahahaha. Go ahead, touch the Cornballer. She's always got to wedge herself in the middle of us so that she can control everything. Yeah. Mom's awesome.`
+);
+
+addText(
+  `No, no, it's pronounced a-nal-ra-pist. It wasn't really the pronunciation. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, asperiores maiores. Ex aliquid unde reiciendis in cum, consectetur necessitatibus? Similique tenetur molestiae, accusamus animi quae neque molestias pariatur dolores maiores sequi quidem expedita modi omnis quod nesciunt, ipsam quis magnam repudiandae. Possimus eligendi facere aliquam suscipit fuga, ullam eos odit!`
+);
+
+addText(
+  `Maybe it was the other George Michael. You know, the singer-songwriter. This was a big get for God. I'm a scholar. I enjoy scholarly pursuits. Suddently playing wits yourself is a scholarly pursuit? Wow. We're just blowing through nap time, aren't we? I need a face passport, preferably to France... I like the way they think. Butterscotch! Want a lick?`
+);
+
+addText(
+  `I made a huge tiny mistake. What's gotten into you? Have you been eating cheese? one of the guys told me to take my head out of my bottom and get back to work... my bottom! Hahahaha. Go ahead, touch the Cornballer. She's always got to wedge herself in the middle of us so that she can control everything. Yeah. Mom's awesome.`
+);
+
+addText(
+  `No, no, it's pronounced a-nal-ra-pist. It wasn't really the pronunciation. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, asperiores maiores. Ex aliquid unde reiciendis in cum, consectetur necessitatibus? Similique tenetur molestiae, accusamus animi quae neque molestias pariatur dolores maiores sequi quidem expedita modi omnis quod nesciunt, ipsam quis magnam repudiandae. Possimus eligendi facere aliquam suscipit fuga, ullam eos odit!`
+);
+
+
